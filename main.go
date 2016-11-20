616355d9cceb55cd579557099b4039fe964f4d9d
andrew gondo <kuro.ketanitem@gmail.com>
2016-11-20 16:09:58 +0700

    add example of serve static and dynamic func and using Port hehe :sweat_smie:

--- main.go
+++ main.go
@@ -1,19 +1,38 @@
-package main;
+package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
 	"net/http"
+	"time"
 )
 
-func testHandler(w http.ResponseWriter, r *http.Request){
+const (
+	port = ":8080"
+)
+
+func testHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func main() {
-	router := mux.NewRouter();
-	router.HandleFunc("/test",testHandler);
+func serveDynamic(w http.ResponseWriter, r *http.Request){
+	response := "The time is now " + time.Now().String();
+	fmt.Fprintln(w, response);
+}
 
-	http.Handle("/",router);
-	fmt.Println("Everything is set up !");
+func serveStatic(w http.ResponseWriter, r *http.Request){
+	//NOTE : file location is relative
+	http.ServeFile(w,r,"public/json/whoami.json");
+}
+
+func serveError(w http.ResponseWriter, r *http.Request){
+	fmt.Println("There's no way way I'll work !");
+}
+
+func main() {
+
+	http.HandleFunc("/test", testHandler)
+	http.HandleFunc("/whoami",serveStatic);
+	http.HandleFunc("/",serveDynamic);
+	http.HandleFunc("/error",serveError)
+	http.ListenAndServe(port,nil);
 }
