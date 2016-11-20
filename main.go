239abb4df79e3ee541752e72d89b3eded3084317
andrew gondo <kuro.ketanitem@gmail.com>
2016-11-20 19:08:26 +0700

    add config to connect to localhost mysql yeay :D

--- main.go
+++ main.go
@@ -1,27 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"github.com/gorilla/mux"
-	"os"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
 )
 
 const (
 	port = ":8081"
+	DBHost = "localhost"
+	DBPort = ":8890"
+	DBUser = "root"
+	DBPass = "root"
+	DBDbase = "cms"
 )
+
+type Page struct{
+	Title string
+	Content string
+	Date string
+}
+
+var database *sql.DB
 
 func pageHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r);
 	pageID := vars["id"];
-	fileName := "public/files/" + pageID + ".html";
 
-	//NOTE : why we need OS stat ? because 404 is a situation based on not found a file
-	_, err := os.Stat(fileName);
+	thisPage := Page{};
+
+	err := database.QueryRow("SELECT page_title,page_content,page_date FROM pages WHERE id=?", pageID).Scan(&thisPage.Title, &thisPage.Content, &thisPage.Date);
 	if err != nil{
-		fileName = "public/files/404.html";
+		log.Println("couldn't get page with id : ",pageID);
+		log.Println(err)
 	}
 
-	http.ServeFile(w,r,fileName);
+	html := `<html><head><title>` + thisPage.Title + `</title></head><body><h1>` + thisPage.Title + `</h1><div>` + thisPage.Content + `</div></body></html>`;
+	fmt.Fprintln(w,html);
 }
 
 func serveStatic(w http.ResponseWriter, r *http.Request){
@@ -29,13 +47,25 @@ func serveStatic(w http.ResponseWriter, r *http.Request){
 	http.ServeFile(w,r,"public/json/whoami.json");
 }
 
+func connectToDB(){
+	dbConn := fmt.Sprintf("%s:%s@tcp(%s%s)/%s",DBUser, DBPass, DBHost,DBPort, DBDbase)
+	fmt.Println(dbConn);
+	db, err := sql.Open("mysql",dbConn)
+
+	if err != nil{
+		log.Println("Couln't connect");
+		log.Fatal(err);
+	}else{
+		log.Println("success connect to db")
+	}
+	database = db;
+}
 
 func main() {
+	connectToDB();
+
 	rtr := mux.NewRouter();
 	rtr.HandleFunc("/pages/{id:[0-9]+}",pageHandler);
-	rtr.HandleFunc("/homepage",pageHandler);
-	rtr.HandleFunc("/contanct",pageHandler);
-
 	http.Handle("/",rtr);
 
 	http.HandleFunc("/whoami",serveStatic);
